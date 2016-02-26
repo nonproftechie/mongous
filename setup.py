7532832857ed1b67422bbe875dafd88958b675db
Joshua Austin <joshthetechie@outlook.com>
2016-02-26 14:26:43 -0600

    Update setup.py for next minor release

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@
 from distutils.core import setup
 
 setup(name='mongous',
-      version='0.1.1',
-      description='A (very) thin Mongo CRUD wrapper library for Python 2',
+      version='0.1.2',
+      description='A (very) thin Mongo CRUD wrapper library',
       author='Joshua Austin',
       author_email='nesher181@gmail.com',
       url='https://github.com/nonproftechie/mongous',
-      download_url='https://github.com/nonproftechie/mongous/tarball/0.1',
+      download_url='https://github.com/nonproftechie/mongous/tarball/0.1.2',
       keywords=['database', 'mongo', 'mongous']
      )
