9788cff47914bc4706fc262a51dc24e98d8799bb
Joshua Austin <joshthetechie@outlook.com>
2016-02-26 13:23:02 -0600

    updated install info for PyPI

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+
+setup(name='mongous',
+      packages=['mongous'],
+      version='0.1',
+      description='A (very) thin Mongo CRUD wrapper library for Python 2',
+      author='Joshua Austin',
+      author_email='nesher181@gmail.com',
+      url='https://github.com/nonproftechie/mongous',
+      download_url='https://github.com/nonproftechie/mongous',
+      keywords=['database', 'mongo', 'mongous']
+     )
