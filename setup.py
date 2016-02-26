ab9a6ad11e8f7b551e822ab8c168e73da9cb72d4
Joshua Austin <joshthetechie@outlook.com>
2016-02-26 13:40:23 -0600

    more setup config work

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@
 from distutils.core import setup
 
 setup(name='mongous',
-      packages=['mongous'],
-      version='0.1',
+      version='0.1.1',
       description='A (very) thin Mongo CRUD wrapper library for Python 2',
       author='Joshua Austin',
       author_email='nesher181@gmail.com',
       url='https://github.com/nonproftechie/mongous',
-      download_url='https://github.com/nonproftechie/mongous',
+      download_url='https://github.com/nonproftechie/mongous/tarball/0.1',
       keywords=['database', 'mongo', 'mongous']
      )
